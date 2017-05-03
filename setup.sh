f2a5a74f9c99351c76b71e6192a182e462374fd8
boeckhoff <nico.boeckhoff@student.hpi.de>
2017-05-03 18:26:27 +0200

    building svg_converter

--- setup.sh
+++ setup.sh
@@ -1,6 +1,4 @@
-mkdir linespace
-cd linespace
-
+apt-get update -y
 apt-get install python-pip -y
 apt-get install python-dev -y
 apt-get install libbluetooth-dev -y
@@ -11,6 +9,18 @@ pip install pybluez
 pip install pyusb
 pip install pint
 
+git clone https://github.com/boeckhoff/linespace_raspberry.git
+cd linespace_raspberry
+
 git clone https://github.com/vishnubob/silhouette.git
 cd silhouette
-pyton setup.py install
+python setup.py install
+cd ..
+
+apt-get install libboost-dev libeigen3-dev libxml2-dev cmake -y
+git clone --recursive https://github.com/Felerius/linespace-svg-simplifier.git
+cd linespace-svg-simplifier
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make svg_converter
