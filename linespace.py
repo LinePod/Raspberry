3a39f8ddfda145be880fcd83bdab76525c801a1c
boeckhoff <nico.boeckhoff@student.hpi.de>
2017-05-03 18:22:39 +0200

    adapted architecture

--- linespace.py
+++ linespace.py
@@ -8,13 +8,13 @@ import usb.core
 import usb.util
 import struct
 import time
+
 LOGFILE_PATH = "logfile"
+APP_UUID = '00001101-0000-1000-8000-00805F9B34FB'
 TEST = True
+
 if len(sys.argv) == 2 and sys.argv[1] == '-t':
     TEST = True
-#eventRegistered1 = 0
-#eventRegistered2 = 0
-#lastSent = time.time()
 
 def restart():
     log("Restarting...")    
@@ -25,16 +25,6 @@ def log(content):
     with open(LOGFILE_PATH,"a+") as output_file:
         output_file.write(time.strftime('%m.%d.%y %H:%M:%S:')+ ' {0}\n'.format(content))
         output_file.close()
-
-def saveSVGToFile(svgString, uuid):
-    svgFile = open("svgs/" + uuid + ".svg")
-    svgFile.write(svgString)
-    svgFile.close()
-
-def convertSVG(uuid):
-    filepath = "svgs/" + uuid + ".svg"
-    out = subprocess.check_output(['linespace-svg-simplifier/build/svg_converter',filepath])
-    print out
 
 class mySilhouette (Silhouette):
     def __init__(self):
@@ -55,21 +45,27 @@ class mySilhouette (Silhouette):
         if not TEST:
             log("Sending to Plotter: " + command)
             super(mySilhouette, self).write(command)
+        else:
+            log("Would send to plotter: " + command)
 
 silhouette = mySilhouette()
 silhouette.config()
 
 class BtCommunication (object):
 
-    def __init__(self, uuid, name):
-        self.server_sock = BluetoothSocket( RFCOMM )
+    def __init__(self, uuid):
+        self.server_sock = BluetoothSocket(RFCOMM)
         self.uuid = uuid
         self.config()
-        self.name = name
-        self.connected = False
+        self.establishConnection()
 
-        self.establishConnectionThread = EstablishConnectionThread(self)
-        self.establishConnectionThread.start()
+    def establishConnection(self):
+        log("Waiting for connection on RFCOMM channel " + str(self.port))
+        self.client_sock, self.client_info = self.server_sock.accept()
+        log("Accepted connection from " + str(self.client_info))
+        log("###############################")
+        log("App is connected")
+        log("###############################")
 
     def config(self):
         os.system("/home/pi/linespace/establishConnection.sh")
@@ -82,35 +78,17 @@ class BtCommunication (object):
                 service_classes = [ self.uuid, SERIAL_PORT_CLASS ],
                 profiles = [ SERIAL_PORT_PROFILE ],)
 
-
-class EstablishConnectionThread(threading.Thread):
-
-    def __init__(self, btObj):
-        self.btObj = btObj
-        super(EstablishConnectionThread, self).__init__()
-
-    def run(self):
-        log("Waiting for connection on RFCOMM channel " + str(self.btObj.port))
-        self.btObj.client_sock, self.btObj.client_info = self.btObj.server_sock.accept()
-        log("Accepted connection from " + str(self.btObj.client_info))
-        self.btObj.connected = True
-        log("###############################")
-        log(self.btObj.name + " is connected")
-        log("###############################")
-
-
 class PrintingThread(threading.Thread):
 
     def __init__(self):
         super(PrintingThread, self).__init__()
 
-    def printGPGLFromUUID(uuid, speed=2):
-        filepath = "gpgls/" + uuid + ".gpgl"
+    def printGPGL(data, speed=2):
         
+        data = open(filepath, 'r').read()
+
         #Set speed and home
         silhouette.ep_out.write('!' + speed + '\x03H\x03')
-
-        data = open(filepath, 'r').read()
 
         for i in range(0, len(data), 1024):
             silhouette.ep_out.write(data[i:i+1024])
@@ -121,11 +99,24 @@ class PrintingThread(threading.Thread):
         #Paper out
         silhouette.ep_out.write('M0,-2000\x03')
 
+    def saveSVGToFile(svgString, uuid):
+        svgFile = open("svg/" + uuid + ".svg")
+        svgFile.write(svgString)
+        svgFile.close()
+
+    def convertSVG(uuid):
+        svgPath = "svg/" + uuid + ".svg"
+        out = subprocess.check_output(['linespace-svg-simplifier/build/svg_converter',svgPath])
+        print out
+        return out
+
     def run(self):
         while True:
             if(len(printingQueue) != 0):
-                uuid = printingQueue.get()
-                printGPGLFromUUID(uuid)
+                [uuid, svgString] = printingQueue.get()
+                self.saveSVGToFile(svgString, uuid)
+                gpglData = self.convertSVG(uuid)
+                self.printGPGL(gpglData)
             time.sleep(.5)
 
 class ListenThread (threading.Thread):
@@ -136,35 +127,15 @@ class ListenThread (threading.Thread):
 
     def run(self):
 
-        buffer = ""
-        curSvg = ""
         try:
             while True:
-                if(self.btObj.connected == True):
-                    data = self.btObj.client_sock.recv(1024)
-                    if len(data) == 0: break
-                    log("received " + str(data))
-
-                    if data[0] == '0' and appCommunication.connected == True:
-                        #if tracking data received
-                        trackingQueue.put(data[2:])
-                        continue
-
-                    if data[0] == 's':
-                        #if svg data received
-                        curSvg += data[1:]
-                        if '\x05' in data:
-                            sendToPlotter(curSvg)
-                            curSvg = ""
-                        continue
-
-                    buffer += data
-
-                    if(data[-1] != '\x04'):
-                        silhouette.write(buffer)
-                        log("logging " + str(len(buffer)))
-                        buffer = ''
-
+                numBytes = struct.unpack(">I", self.btObj.client_sock.recv(4))
+                if numBytes == 0: break
+                uuid = self.btObj.client_sock.recv(36)
+                svgData = self.btObj.client_sock.recv(numBytes)
+                printingQueue.put([uuid, svgData])
+                
+                log("received SVG with uuid: " + uuid + " size: " + numBytes) 
                 time.sleep(0.01)
 
         except IOError:
@@ -176,37 +147,23 @@ class ListenThread (threading.Thread):
         self.btObj.server_sock.close()
         return
 
-"""class SendThread(threading.Thread):
+class SendThread(threading.Thread):
 
     def __init__(self, btObj):
         self.btObj = btObj
         super(SendThread, self).__init__()
 
-
     def run(self):
 
-        toSend = ''
-
         while True:
-            startTime = time.time()
-            if(self.btObj.connected == True):
-
-                if(toSend == ''):
-                     toSend = trackingQueue.get()
-                self.btObj.client_sock.send(toSend)
-                log("Send: " + toSend)
-                toSend = ''
-
-                global lastSent
-                global eventRegistered1
-                global eventRegistered2
-                lastSent = time.time()
-                eventRegistered1 = 0
-                eventRegistered2 = 0
-            time.sleep(0.01)
-            log(time.time() -startTime)"""
+            toSend = sendingQueue.get()
+            self.btObj.client_sock.send(toSend)
+            log("Send: " + toSend)
 
 class TrackingThread(threading.Thread):
+    '''
+    Receive tracking Data and put into sendingQueue
+    '''
 
     lastEvent1 = -1
     lastEvent2 = -1
@@ -227,8 +184,6 @@ class TrackingThread(threading.Thread):
         if self.dev.is_kernel_driver_active(self.interface) is True:
             self.dev.detach_kernel_driver(self.interface)
             usb.util.claim_interface(self.dev,self.interface)
-
-
 
     def getEventType(self, newEvent1,newEvent2):
         event1=-1
@@ -262,58 +217,57 @@ class TrackingThread(threading.Thread):
         count=0
         while True:
             try:
-                if (self.btObj.connected==True):
-                    data=self.dev.read(0x81,0x40)
-                    finger_left_x1=data[5]
-                    finger_left_x2=data[6]
-                    finger_left_y1=data[7]
-                    finger_left_y2=data[8]
-                    x_left=finger_left_x2*256+finger_left_x1
-                    y_left=finger_left_y2*256+finger_left_y1
+                data=self.dev.read(0x81,0x40)
+                finger_left_x1=data[5]
+                finger_left_x2=data[6]
+                finger_left_y1=data[7]
+                finger_left_y2=data[8]
+                x_left=finger_left_x2*256+finger_left_x1
+                y_left=finger_left_y2*256+finger_left_y1
 
-                    finger_right_x1=data[14]
-                    finger_right_x2=data[15]
-                    finger_right_y1=data[16]
-                    finger_right_y2=data[17]
-                    x_right=finger_right_x2*256+finger_right_x1
-                    y_right=finger_right_y2*256+finger_right_y1
+                finger_right_x1=data[14]
+                finger_right_x2=data[15]
+                finger_right_y1=data[16]
+                finger_right_y2=data[17]
+                x_right=finger_right_x2*256+finger_right_x1
+                y_right=finger_right_y2*256+finger_right_y1
 
-                    eventTypes = self.getEventType(data[4],data[13])
+                eventTypes = self.getEventType(data[4],data[13])
 
-                    bytesToSend = struct.pack(">6i",x_left,y_left,x_right,y_right,eventTypes[0],eventTypes[1])
+                bytesToSend = struct.pack(">6i",x_left,y_left,x_right,y_right,eventTypes[0],eventTypes[1])
 
-                    self.btObj.client_sock.send(bytesToSend)
-                    log("Sent bytes")
+                if (eventTypes[0] == 1 or eventTypes[0] == 2) or (eventTypes[1] == 1 or eventTypes[1] == 2):
+                    sendingQueue.put(bytesToSend)
+                else:
+                    count +=1
 
-                    print str(x_left) + "  " + str(y_left)
-                    """if count >5:
-                            count=0
-                            #trackingQueue.put_nowait(str(x_left)+","+str(y_left)+","+str(x_right)+","+str(y_right))
-                            trackingQueue.put_nowait(bytesToSend)"""
+                if count > 10:
+                    count = 0
+                    sendingQueue.put(bytesToSend)
 
             except usb.core.USBError as e:
                 data = None
                 continue
                 count+=1
 
-uuidDict = {'app' : '00001101-0000-1000-8000-00805F9B34FB',
-            'tracking' : '00001101-9999-1000-8000-00805F9B34FB'}
 
-trackingQueue = Queue()
-appCommunication = BtCommunication(uuidDict['app'], 'app')
-trackingCommunication = BtCommunication(uuidDict['tracking'], 'tracking')
+printingQueue = Queue()
+sendingQueue = Queue()
 
-appListenThread = ListenThread(appCommunication)
-#trackingListenThread = ListenThread(trackingCommunication)
+appCommunication = BtCommunication(APP_UUID)
+
+listenThread = ListenThread(appCommunication)
+sendThread = SendThread(appCommunication)
+trackingThread = TrackingThread(appCommunication)
+printingThread = PrintingThread()
+
 log("starting")
-appListenThread.start()
-#appSendThread = SendThread(appCommunication)
-#appSendThread.start()
-trackingGatherThread= TrackingThread(appCommunication)
-trackingGatherThread.start()
-#trackingListenThread.start()
+listenThread.start()
+sendThread.start()
+trackingThread.start()
+printingThread.start()
 
-trackingGatherThread.join()
-appListenThread.join()
-appSendThread.join()
-#trackingListenThread.join()
+trackingThread.join()
+listenThread.join()
+sendThread.join()
+printingThread.join()
