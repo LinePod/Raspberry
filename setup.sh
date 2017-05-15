31e149ab57f2b2e9ec59865ede6fa38b31d0a0f1
David Stangl <david.stangl@student.hpi.de>
2017-05-15 12:35:00 +0200

    Include SSH key setup in install script

--- setup.sh
+++ setup.sh
@@ -2,13 +2,14 @@
 
 # This script is supposed to be run on a clean machine and will handle all
 # setup. The first argument needs to be the path to a directory with the
-# needed ssh keys to clone all repositories. They should be copied onto the
-# machine and been given the correct permissions so that ssh won't complain
-# (for example `chmod -R 600 /keys`). The folder will be deleted by this
-# script after setup.
+# needed ssh keys to clone all repositories. They will be copied into a
+# temporary directory, given the right access rights so that ssh doesn't
+# complain and deleted later (only the copy).
 
-# Use the SSH key for all git commands
-export SSH_KEY_DIR="$1"
+# Setup ssh keys
+export SSH_KEY_DIR=$(mktemp -d)
+cp -R "${1}/." $SSH_KEY_DIR
+chmod -R 600 $SSH_KEY_DIR
 
 apt-get update -y
 apt-get install -y git
